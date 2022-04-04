add8bcf96cf653400c9e7a8b834fbc6573ca2134
CsobothEniko <enikc016@gmail.com>
2022-04-04 11:52:42 +0200

    login + registration field

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Hello Home</h1>
+</template>
+
+<script>
+
+export default {
+    name: 'Home'
+}
+</script>
---
+++ src/components/Registration.vue
@@ -0,0 +1,57 @@
+<template>
+<form @submit.prevent="handleSubmit">
+    <h3> Registration </h3>
+
+    <div class="form-group">
+        <label> Name </label>
+        <input type="name" class="form-control" v-model="name" placeholder="name" />
+    </div>
+
+    <div class="form-group">
+        <label> Username </label>
+        <input type="username" class="form-control" v-model="username" placeholder="Username" />
+    </div>
+
+    <div class="form-group">
+        <label> Email </label>
+        <input type="email" class="form-control" v-model="email" placeholder="email" />
+    </div>
+
+    <div class="form-group">
+        <label> Password </label>
+        <input type="password" class="form-control" v-model="password" placeholder="Password" />
+    </div>
+
+    <div class="form-group">
+        <label> Confirm Password </label>
+        <input type="password" class="form-control" v-model="password_confirm" placeholder="Confirm Password" />
+    </div>
+
+
+    <button class="btn btn-primary btn-block">Registration </button>
+</form>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Registration',
+    data(){
+        return {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+            password_confirm : ''
+        }
+    },
+    methods:{
+        handleSubmit(){
+            //így megjelenik a szöveg, nem frissíti folyamat az oldalt
+            //e.preventDefault();
+            console.log('submitted!!!');
+        }
+    }
+}
+</script>
